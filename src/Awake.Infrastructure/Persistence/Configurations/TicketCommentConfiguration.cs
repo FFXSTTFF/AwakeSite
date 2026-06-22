f2cb9dafacde661986501cb6f5285fecfb7e7ec6
SystemX <sp3664810@gmail.com>
2026-06-22 11:58:29 +0300

    fix(infrastructure): correct TicketComment.Content max length to 2000

--- src/Awake.Infrastructure/Persistence/Configurations/TicketCommentConfiguration.cs
+++ src/Awake.Infrastructure/Persistence/Configurations/TicketCommentConfiguration.cs
@@ -22,6 +22,6 @@ public class TicketCommentConfiguration : IEntityTypeConfiguration<TicketComment
 
         builder.Property(x => x.Content)
             .IsRequired()
-            .HasMaxLength(1000);
+            .HasMaxLength(2000);
     }
 }
